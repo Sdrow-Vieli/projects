2dac4c49234ccc9f4616be773f1042e9fc0e837b
Lindo-code <sdrowvieli1@gmail.com>
2026-03-29 15:50:21 +0200

    updated background

--- components/common/ChipBackground.jsx
+++ components/common/ChipBackground.jsx
@@ -1,188 +1,68 @@
+"use client";
+
 export default function ChipBackground() {
   return (
     <div className="chip-bg" aria-hidden="true">
       <svg
         id="chip-bg"
         viewBox="0 0 1440 1024"
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMidYMid slice"
       >
         <g className="pcb-frame">
-          {/* TOP LEFT */}
-          <path className="trace" d="M0 40 H120 L150 70 V120" />
-          <circle className="node" cx="150" cy="120" r="4" />
+          {/* TOP */}
+          <path className="trace" d="M0 60 H180 L220 100 H340" />
+          <circle className="node" cx="340" cy="100" r="4" />
 
-          <path className="trace" d="M0 70 H90 L130 110 V150" />
-          <circle className="node" cx="130" cy="150" r="4" />
+          <path className="trace" d="M240 0 V60 L300 120 H420" />
+          <circle className="node" cx="420" cy="120" r="4" />
 
-          <path className="trace" d="M0 105 H150 L185 140 H250" />
-          <circle className="node" cx="250" cy="140" r="4" />
+          <path className="trace" d="M500 0 V80 L560 140 H680" />
+          <circle className="node" cx="680" cy="140" r="4" />
 
-          <path className="trace" d="M0 145 H115 L150 180 H220" />
-          <circle className="node" cx="220" cy="180" r="4" />
+          <path className="trace" d="M760 0 V70 L820 130 H940" />
+          <circle className="node" cx="940" cy="130" r="4" />
 
-          <path className="trace" d="M0 185 H95 L130 220 H260" />
-          <circle className="node" cx="260" cy="220" r="4" />
+          <path className="trace" d="M1440 80 H1260 L1210 140 H1100" />
+          <circle className="node" cx="1100" cy="140" r="4" />
 
-          <path className="trace" d="M0 225 H180 L220 265 H320" />
-          <circle className="node" cx="320" cy="265" r="4" />
+          {/* LEFT */}
+          <path className="trace" d="M0 240 H160 L210 290 H320" />
+          <circle className="node" cx="320" cy="290" r="4" />
 
-          {/* TOP MID LEFT */}
-          <path className="trace" d="M260 0 V55 L315 110 H400" />
-          <circle className="node" cx="400" cy="110" r="4" />
+          <path className="trace" d="M0 400 H120 L170 450 H300" />
+          <circle className="node" cx="300" cy="450" r="4" />
 
-          <path className="trace" d="M305 0 V45 L360 100 H445" />
-          <circle className="node" cx="445" cy="100" r="4" />
+          <path className="trace" d="M0 560 H140 L190 610 H310" />
+          <circle className="node" cx="310" cy="610" r="4" />
 
-          <path className="trace" d="M350 0 V65 L420 135 H500" />
-          <circle className="node" cx="500" cy="135" r="4" />
+          <path className="trace" d="M0 720 H100 L150 770 H260" />
+          <circle className="node" cx="260" cy="770" r="4" />
 
-          <path className="trace" d="M395 0 V40 L450 95 H530" />
-          <circle className="node" cx="530" cy="95" r="4" />
+          {/* RIGHT */}
+          <path className="trace" d="M1440 260 H1280 L1230 310 H1120" />
+          <circle className="node" cx="1120" cy="310" r="4" />
 
-          <path className="trace" d="M450 0 V50 L520 120 H610" />
-          <circle className="node" cx="610" cy="120" r="4" />
+          <path className="trace" d="M1440 420 H1300 L1250 470 H1150" />
+          <circle className="node" cx="1150" cy="470" r="4" />
 
-          {/* TOP MID */}
-          <path className="trace" d="M580 0 V40 L620 80 V130" />
-          <circle className="node" cx="620" cy="130" r="4" />
+          <path className="trace" d="M1440 600 H1290 L1240 650 H1140" />
+          <circle className="node" cx="1140" cy="650" r="4" />
 
-          <path className="trace" d="M640 0 V30 L680 70 V115" />
-          <circle className="node" cx="680" cy="115" r="4" />
+          <path className="trace" d="M1440 760 H1310 L1260 810 H1160" />
+          <circle className="node" cx="1160" cy="810" r="4" />
 
-          <path className="trace" d="M700 0 V45 L745 90 H820" />
-          <circle className="node" cx="820" cy="90" r="4" />
+          {/* BOTTOM */}
+          <path className="trace" d="M0 980 H180 L240 920 H340" />
+          <circle className="node" cx="340" cy="920" r="4" />
 
-          <path className="trace" d="M760 0 V55 L815 110 H900" />
-          <circle className="node" cx="900" cy="110" r="4" />
+          <path className="trace" d="M420 1024 V960 L480 900 H600" />
+          <circle className="node" cx="600" cy="900" r="4" />
 
-          {/* TOP MID RIGHT */}
-          <path className="trace" d="M910 0 V50 L965 105 H1045" />
-          <circle className="node" cx="1045" cy="105" r="4" />
+          <path className="trace" d="M900 1024 V960 L840 900 H720" />
+          <circle className="node" cx="720" cy="900" r="4" />
 
-          <path className="trace" d="M965 0 V40 L1020 95 H1105" />
-          <circle className="node" cx="1105" cy="95" r="4" />
-
-          <path className="trace" d="M1025 0 V60 L1090 125 H1185" />
-          <circle className="node" cx="1185" cy="125" r="4" />
-
-          <path className="trace" d="M1085 0 V35 L1150 100 H1245" />
-          <circle className="node" cx="1245" cy="100" r="4" />
-
-          {/* TOP RIGHT */}
-          <path className="trace" d="M1440 45 H1325 L1285 85 H1205" />
-          <circle className="node" cx="1205" cy="85" r="4" />
-
-          <path className="trace" d="M1440 85 H1355 L1315 125 H1230" />
-          <circle className="node" cx="1230" cy="125" r="4" />
-
-          <path className="trace" d="M1440 125 H1300 L1260 165 H1165" />
-          <circle className="node" cx="1165" cy="165" r="4" />
-
-          <path className="trace" d="M1440 165 H1340 L1290 215 H1180" />
-          <circle className="node" cx="1180" cy="215" r="4" />
-
-          <path className="trace" d="M1440 205 H1360 L1320 245 H1215" />
-          <circle className="node" cx="1215" cy="245" r="4" />
-
-          <path className="trace" d="M1440 245 H1295 L1250 290 H1175" />
-          <circle className="node" cx="1175" cy="290" r="4" />
-
-          {/* LEFT SIDE */}
-          <path className="trace" d="M0 320 H110 L145 355 H280" />
-          <circle className="node" cx="280" cy="355" r="4" />
-
-          <path className="trace" d="M0 380 H80 L120 420 H240" />
-          <circle className="node" cx="240" cy="420" r="4" />
-
-          <path className="trace" d="M0 450 H100 L140 490 H270" />
-          <circle className="node" cx="270" cy="490" r="4" />
-
-          <path className="trace" d="M0 520 H95 L135 560 H255" />
-          <circle className="node" cx="255" cy="560" r="4" />
-
-          <path className="trace" d="M0 590 H120 L160 630 H275" />
-          <circle className="node" cx="275" cy="630" r="4" />
-
-          <path className="trace" d="M0 660 H85 L120 695 H215" />
-          <circle className="node" cx="215" cy="695" r="4" />
-
-          {/* RIGHT SIDE */}
-          <path className="trace" d="M1440 350 H1320 L1280 390 H1160" />
-          <circle className="node" cx="1160" cy="390" r="4" />
-
-          <path className="trace" d="M1440 420 H1350 L1310 460 H1195" />
-          <circle className="node" cx="1195" cy="460" r="4" />
-
-          <path className="trace" d="M1440 495 H1305 L1260 540 H1155" />
-          <circle className="node" cx="1155" cy="540" r="4" />
-
-          <path className="trace" d="M1440 565 H1330 L1285 610 H1170" />
-          <circle className="node" cx="1170" cy="610" r="4" />
-
-          <path className="trace" d="M1440 640 H1360 L1325 675 H1210" />
-          <circle className="node" cx="1210" cy="675" r="4" />
-
-          <path className="trace" d="M1440 710 H1315 L1270 755 H1165" />
-          <circle className="node" cx="1165" cy="755" r="4" />
-
-          {/* BOTTOM LEFT */}
-          <path className="trace" d="M0 980 H130 L170 940 H255" />
-          <circle className="node" cx="255" cy="940" r="4" />
-
-          <path className="trace" d="M0 940 H105 L145 900 H230" />
-          <circle className="node" cx="230" cy="900" r="4" />
-
-          <path className="trace" d="M0 900 H155 L205 850 H315" />
-          <circle className="node" cx="315" cy="850" r="4" />
-
-          <path className="trace" d="M0 860 H95 L140 815 H250" />
-          <circle className="node" cx="250" cy="815" r="4" />
-
-          <path className="trace" d="M0 820 H120 L165 775 H270" />
-          <circle className="node" cx="270" cy="775" r="4" />
-
-          {/* BOTTOM MID LEFT */}
-          <path className="trace" d="M360 1024 V950 L420 890 H505" />
-          <circle className="node" cx="505" cy="890" r="4" />
-
-          <path className="trace" d="M420 1024 V965 L475 910 H560" />
-          <circle className="node" cx="560" cy="910" r="4" />
-
-          <path className="trace" d="M480 1024 V940 L545 875 H625" />
-          <circle className="node" cx="625" cy="875" r="4" />
-
-          <path className="trace" d="M545 1024 V955 L600 900 H675" />
-          <circle className="node" cx="675" cy="900" r="4" />
-
-          {/* BOTTOM MID RIGHT */}
-          <path className="trace" d="M900 1024 V955 L845 900 H765" />
-          <circle className="node" cx="765" cy="900" r="4" />
-
-          <path className="trace" d="M965 1024 V940 L900 875 H820" />
-          <circle className="node" cx="820" cy="875" r="4" />
-
-          <path className="trace" d="M1030 1024 V965 L975 910 H890" />
-          <circle className="node" cx="890" cy="910" r="4" />
-
-          <path className="trace" d="M1090 1024 V950 L1035 895 H950" />
-          <circle className="node" cx="950" cy="895" r="4" />
-
-          {/* BOTTOM RIGHT */}
-          <path className="trace" d="M1440 980 H1315 L1275 940 H1180" />
-          <circle className="node" cx="1180" cy="940" r="4" />
-
-          <path className="trace" d="M1440 940 H1340 L1295 895 H1205" />
-          <circle className="node" cx="1205" cy="895" r="4" />
-
-          <path className="trace" d="M1440 900 H1290 L1245 850 H1140" />
-          <circle className="node" cx="1140" cy="850" r="4" />
-
-          <path className="trace" d="M1440 860 H1355 L1310 815 H1210" />
-          <circle className="node" cx="1210" cy="815" r="4" />
-
-          <path className="trace" d="M1440 820 H1325 L1285 775 H1190" />
-          <circle className="node" cx="1190" cy="775" r="4" />
+          <path className="trace" d="M1440 960 H1280 L1230 900 H1120" />
+          <circle className="node" cx="1120" cy="900" r="4" />
         </g>
       </svg>
     </div>
--- components/layouts/DefaultLayout.jsx
+++ components/layouts/DefaultLayout.jsx
@@ -3,36 +3,13 @@
 import Navbar from "../common/Navbar";
 import Sidebar from "../common/Sidebar";
 import Footer from "../common/Footer";
+import ChipBackground from "../common/ChipBackground";
 import "./Layouts.css";
 
 export default function DefaultLayout({ children }) {
   return (
     <div className="default-layout">
-      <div className="chip-bg" aria-hidden="true">
-        <svg
-          id="chip-bg"
-          viewBox="0 0 1200 800"
-          preserveAspectRatio="xMidYMid slice"
-        >
-          <g className="group">
-            <path className="trace" d="M120 220 H420 Q470 220 470 270 V360" />
-            <path
-              className="trace delay-1"
-              d="M760 180 H560 Q510 180 510 230 V320"
-            />
-            <path
-              className="trace delay-2"
-              d="M240 560 H540 Q600 560 600 500 V420"
-            />
-
-            <circle className="node" cx="420" cy="220" r="6" />
-            <circle className="node" cx="470" cy="360" r="6" />
-            <circle className="node" cx="760" cy="180" r="6" />
-            <circle className="node" cx="600" cy="420" r="6" />
-          </g>
-        </svg>
-      </div>
-
+      <ChipBackground />
       <Navbar />
       <main className="main">
         <div className="container">
