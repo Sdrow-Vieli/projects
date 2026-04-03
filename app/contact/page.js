1c883559e7a913917074a0e8a32c8f895098ea7a
Lindo-code <sdrowvieli1@gmail.com>
2026-04-03 14:44:21 +0200

    updated

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,16 +2,13 @@
 
 import FullWidthLayout from "@/components/layouts/FullWidthLayout";
 import NeatAltStack from "@/components/NeatAltStack";
-import NeatAltStackGrouped from "@/components/NeatStack";
-
-import cardsData from "@/data/projects.json";
 
 export default function Projects() {
   return (
     <FullWidthLayout
       showHero={true}
       heroProps={{
-        title: "Contact",
+        title: "Get In Touch",
         subtitle:
           "Explore our portfolio of projects across web, mobile, and gaming platforms",
         showIntro: true,
